18468ad24cef46b595fba00702b283e41885a960
Ben <70361638+SpecialAgentB3@users.noreply.github.com>
2025-02-26 14:56:34 -0600

    one more change

--- components/button.js
+++ components/button.js
@@ -1,7 +1,7 @@
 "use client";
 
 const Button = () => {
-    return <button onClick={() => alert("STOP!!!")}>Click Click Click</button>;
+    return <button onClick={() => alert("STOP!!!")}>CLICK CLICK CLICK</button>;
 };
 
 export default Button;
